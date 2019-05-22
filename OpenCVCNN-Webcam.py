1cc3e15b37200c87aab0bbce41317711b8073d86
HuyNX <nguyenhuyvnu@gmail.com>
2019-05-22 16:17:37 +0700

    Manual build FaceNet model

--- OpenCVCNN-Webcam.py
+++ OpenCVCNN-Webcam.py
@@ -1,1 +1,393 @@
-# HuyNX
+from __future__ import division
+import cv2
+import time
+import sys
+import numpy as np
+import glob
+import os
+from keras.models import load_model
+import FaceNet
+import tensorflow as tf
+from keras.preprocessing.image import img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
+from keras.models import Model
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+from keras.layers.merge import Concatenate
+from keras.layers.core import Lambda, Flatten, Dense
+from keras.engine.topology import Layer
+from keras import backend as K
+import cv2
+import os
+import numpy as np
+from numpy import genfromtxt
+import pandas as pd
+import tensorflow as tf
+from utils import LRN2D
+import utils
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+def detectFaceOpenCVDnn(net, frame):
+    frameOpencvDnn = frame.copy()
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
+    blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], False, False)
+
+    net.setInput(blob)
+    detections = net.forward()
+    bboxes = []
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            bboxes.append([x1, y1, x2, y2])
+            # cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn, bboxes
+
+
+def image_to_embedding(image, model):
+    # image = cv2.resize(image, (96, 96), interpolation=cv2.INTER_AREA)
+    image = cv2.resize(image, (96, 96))
+    img = image[..., ::-1]
+    img = np.around(np.transpose(img, (0, 1, 2)) / 255.0, decimals=12)
+    x_train = np.array([img])
+    embedding = model.predict_on_batch(x_train)
+    return embedding
+
+
+# ## About <font color=blue>recognize_face</font> function
+# This function calculate similarity between the captured image and the images that are already been stored. It passes the image to the trained neural network to generate its embedding vector. Which is then compared with all the embedding vectors of the images stored by calculating L2 Euclidean distance.
+#
+# If the minimum L2 distance between two embeddings is less than a threshpld (here I have taken the threashhold as .68 (which can be adjusted) then we have a match.
+
+# In[5]:
+
+
+def recognize_face(face_image, input_embeddings, model):
+    embedding = image_to_embedding(face_image, model)
+
+    minimum_distance = 200
+    name = None
+
+    # Loop over  names and encodings.
+    for (input_name, input_embedding) in input_embeddings.items():
+
+        euclidean_distance = np.linalg.norm(embedding - input_embedding)
+
+        print('Euclidean distance from %s is %s' % (input_name, euclidean_distance))
+
+        if euclidean_distance < minimum_distance:
+            minimum_distance = euclidean_distance
+            name = input_name
+
+    if minimum_distance < 0.68:
+        return str(name)
+    else:
+        return None
+
+def create_input_image_embeddings(model):
+    input_embeddings = {}
+
+    for file in glob.glob("images/*"):
+        person_name = os.path.splitext(os.path.basename(file))[0]
+        image_file = cv2.imread(file, 1)
+        input_embeddings[person_name] = image_to_embedding(image_file, model)
+
+    return input_embeddings
+
+if __name__ == "__main__" :
+
+    myInput = Input(shape=(96, 96, 3))
+
+    x = ZeroPadding2D(padding=(3, 3), input_shape=(96, 96, 3))(myInput)
+    x = Conv2D(64, (7, 7), strides=(2, 2), name='conv1')(x)
+    x = BatchNormalization(axis=3, epsilon=0.00001, name='bn1')(x)
+    x = Activation('relu')(x)
+    x = ZeroPadding2D(padding=(1, 1))(x)
+    x = MaxPooling2D(pool_size=3, strides=2)(x)
+    x = Lambda(LRN2D, name='lrn_1')(x)
+    x = Conv2D(64, (1, 1), name='conv2')(x)
+    x = BatchNormalization(axis=3, epsilon=0.00001, name='bn2')(x)
+    x = Activation('relu')(x)
+    x = ZeroPadding2D(padding=(1, 1))(x)
+    x = Conv2D(192, (3, 3), name='conv3')(x)
+    x = BatchNormalization(axis=3, epsilon=0.00001, name='bn3')(x)
+    x = Activation('relu')(x)
+    x = Lambda(LRN2D, name='lrn_2')(x)
+    x = ZeroPadding2D(padding=(1, 1))(x)
+    x = MaxPooling2D(pool_size=3, strides=2)(x)
+
+    # Inception3a
+    inception_3a_3x3 = Conv2D(96, (1, 1), name='inception_3a_3x3_conv1')(x)
+    inception_3a_3x3 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3a_3x3_bn1')(inception_3a_3x3)
+    inception_3a_3x3 = Activation('relu')(inception_3a_3x3)
+    inception_3a_3x3 = ZeroPadding2D(padding=(1, 1))(inception_3a_3x3)
+    inception_3a_3x3 = Conv2D(128, (3, 3), name='inception_3a_3x3_conv2')(inception_3a_3x3)
+    inception_3a_3x3 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3a_3x3_bn2')(inception_3a_3x3)
+    inception_3a_3x3 = Activation('relu')(inception_3a_3x3)
+
+    inception_3a_5x5 = Conv2D(16, (1, 1), name='inception_3a_5x5_conv1')(x)
+    inception_3a_5x5 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3a_5x5_bn1')(inception_3a_5x5)
+    inception_3a_5x5 = Activation('relu')(inception_3a_5x5)
+    inception_3a_5x5 = ZeroPadding2D(padding=(2, 2))(inception_3a_5x5)
+    inception_3a_5x5 = Conv2D(32, (5, 5), name='inception_3a_5x5_conv2')(inception_3a_5x5)
+    inception_3a_5x5 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3a_5x5_bn2')(inception_3a_5x5)
+    inception_3a_5x5 = Activation('relu')(inception_3a_5x5)
+
+    inception_3a_pool = MaxPooling2D(pool_size=3, strides=2)(x)
+    inception_3a_pool = Conv2D(32, (1, 1), name='inception_3a_pool_conv')(inception_3a_pool)
+    inception_3a_pool = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3a_pool_bn')(inception_3a_pool)
+    inception_3a_pool = Activation('relu')(inception_3a_pool)
+    inception_3a_pool = ZeroPadding2D(padding=((3, 4), (3, 4)))(inception_3a_pool)
+
+    inception_3a_1x1 = Conv2D(64, (1, 1), name='inception_3a_1x1_conv')(x)
+    inception_3a_1x1 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3a_1x1_bn')(inception_3a_1x1)
+    inception_3a_1x1 = Activation('relu')(inception_3a_1x1)
+
+    inception_3a = concatenate([inception_3a_3x3, inception_3a_5x5, inception_3a_pool, inception_3a_1x1], axis=3)
+
+    # Inception3b
+    inception_3b_3x3 = Conv2D(96, (1, 1), name='inception_3b_3x3_conv1')(inception_3a)
+    inception_3b_3x3 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3b_3x3_bn1')(inception_3b_3x3)
+    inception_3b_3x3 = Activation('relu')(inception_3b_3x3)
+    inception_3b_3x3 = ZeroPadding2D(padding=(1, 1))(inception_3b_3x3)
+    inception_3b_3x3 = Conv2D(128, (3, 3), name='inception_3b_3x3_conv2')(inception_3b_3x3)
+    inception_3b_3x3 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3b_3x3_bn2')(inception_3b_3x3)
+    inception_3b_3x3 = Activation('relu')(inception_3b_3x3)
+
+    inception_3b_5x5 = Conv2D(32, (1, 1), name='inception_3b_5x5_conv1')(inception_3a)
+    inception_3b_5x5 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3b_5x5_bn1')(inception_3b_5x5)
+    inception_3b_5x5 = Activation('relu')(inception_3b_5x5)
+    inception_3b_5x5 = ZeroPadding2D(padding=(2, 2))(inception_3b_5x5)
+    inception_3b_5x5 = Conv2D(64, (5, 5), name='inception_3b_5x5_conv2')(inception_3b_5x5)
+    inception_3b_5x5 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3b_5x5_bn2')(inception_3b_5x5)
+    inception_3b_5x5 = Activation('relu')(inception_3b_5x5)
+
+    inception_3b_pool = Lambda(lambda x: x ** 2, name='power2_3b')(inception_3a)
+    inception_3b_pool = AveragePooling2D(pool_size=(3, 3), strides=(3, 3))(inception_3b_pool)
+    inception_3b_pool = Lambda(lambda x: x * 9, name='mult9_3b')(inception_3b_pool)
+    inception_3b_pool = Lambda(lambda x: K.sqrt(x), name='sqrt_3b')(inception_3b_pool)
+    inception_3b_pool = Conv2D(64, (1, 1), name='inception_3b_pool_conv')(inception_3b_pool)
+    inception_3b_pool = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3b_pool_bn')(inception_3b_pool)
+    inception_3b_pool = Activation('relu')(inception_3b_pool)
+    inception_3b_pool = ZeroPadding2D(padding=(4, 4))(inception_3b_pool)
+
+    inception_3b_1x1 = Conv2D(64, (1, 1), name='inception_3b_1x1_conv')(inception_3a)
+    inception_3b_1x1 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3b_1x1_bn')(inception_3b_1x1)
+    inception_3b_1x1 = Activation('relu')(inception_3b_1x1)
+
+    inception_3b = concatenate([inception_3b_3x3, inception_3b_5x5, inception_3b_pool, inception_3b_1x1], axis=3)
+
+    # Inception3c
+    inception_3c_3x3 = utils.conv2d_bn(inception_3b,
+                                       layer='inception_3c_3x3',
+                                       cv1_out=128,
+                                       cv1_filter=(1, 1),
+                                       cv2_out=256,
+                                       cv2_filter=(3, 3),
+                                       cv2_strides=(2, 2),
+                                       padding=(1, 1))
+
+    inception_3c_5x5 = utils.conv2d_bn(inception_3b,
+                                       layer='inception_3c_5x5',
+                                       cv1_out=32,
+                                       cv1_filter=(1, 1),
+                                       cv2_out=64,
+                                       cv2_filter=(5, 5),
+                                       cv2_strides=(2, 2),
+                                       padding=(2, 2))
+
+    inception_3c_pool = MaxPooling2D(pool_size=3, strides=2)(inception_3b)
+    inception_3c_pool = ZeroPadding2D(padding=((0, 1), (0, 1)))(inception_3c_pool)
+
+    inception_3c = concatenate([inception_3c_3x3, inception_3c_5x5, inception_3c_pool], axis=3)
+
+    # inception 4a
+    inception_4a_3x3 = utils.conv2d_bn(inception_3c,
+                                       layer='inception_4a_3x3',
+                                       cv1_out=96,
+                                       cv1_filter=(1, 1),
+                                       cv2_out=192,
+                                       cv2_filter=(3, 3),
+                                       cv2_strides=(1, 1),
+                                       padding=(1, 1))
+    inception_4a_5x5 = utils.conv2d_bn(inception_3c,
+                                       layer='inception_4a_5x5',
+                                       cv1_out=32,
+                                       cv1_filter=(1, 1),
+                                       cv2_out=64,
+                                       cv2_filter=(5, 5),
+                                       cv2_strides=(1, 1),
+                                       padding=(2, 2))
+
+    inception_4a_pool = Lambda(lambda x: x ** 2, name='power2_4a')(inception_3c)
+    inception_4a_pool = AveragePooling2D(pool_size=(3, 3), strides=(3, 3))(inception_4a_pool)
+    inception_4a_pool = Lambda(lambda x: x * 9, name='mult9_4a')(inception_4a_pool)
+    inception_4a_pool = Lambda(lambda x: K.sqrt(x), name='sqrt_4a')(inception_4a_pool)
+    inception_4a_pool = utils.conv2d_bn(inception_4a_pool,
+                                        layer='inception_4a_pool',
+                                        cv1_out=128,
+                                        cv1_filter=(1, 1),
+                                        padding=(2, 2))
+    inception_4a_1x1 = utils.conv2d_bn(inception_3c,
+                                       layer='inception_4a_1x1',
+                                       cv1_out=256,
+                                       cv1_filter=(1, 1))
+    inception_4a = concatenate([inception_4a_3x3, inception_4a_5x5, inception_4a_pool, inception_4a_1x1], axis=3)
+
+    # inception4e
+    inception_4e_3x3 = utils.conv2d_bn(inception_4a,
+                                       layer='inception_4e_3x3',
+                                       cv1_out=160,
+                                       cv1_filter=(1, 1),
+                                       cv2_out=256,
+                                       cv2_filter=(3, 3),
+                                       cv2_strides=(2, 2),
+                                       padding=(1, 1))
+    inception_4e_5x5 = utils.conv2d_bn(inception_4a,
+                                       layer='inception_4e_5x5',
+                                       cv1_out=64,
+                                       cv1_filter=(1, 1),
+                                       cv2_out=128,
+                                       cv2_filter=(5, 5),
+                                       cv2_strides=(2, 2),
+                                       padding=(2, 2))
+    inception_4e_pool = MaxPooling2D(pool_size=3, strides=2)(inception_4a)
+    inception_4e_pool = ZeroPadding2D(padding=((0, 1), (0, 1)))(inception_4e_pool)
+
+    inception_4e = concatenate([inception_4e_3x3, inception_4e_5x5, inception_4e_pool], axis=3)
+
+    # inception5a
+    inception_5a_3x3 = utils.conv2d_bn(inception_4e,
+                                       layer='inception_5a_3x3',
+                                       cv1_out=96,
+                                       cv1_filter=(1, 1),
+                                       cv2_out=384,
+                                       cv2_filter=(3, 3),
+                                       cv2_strides=(1, 1),
+                                       padding=(1, 1))
+
+    inception_5a_pool = Lambda(lambda x: x ** 2, name='power2_5a')(inception_4e)
+    inception_5a_pool = AveragePooling2D(pool_size=(3, 3), strides=(3, 3))(inception_5a_pool)
+    inception_5a_pool = Lambda(lambda x: x * 9, name='mult9_5a')(inception_5a_pool)
+    inception_5a_pool = Lambda(lambda x: K.sqrt(x), name='sqrt_5a')(inception_5a_pool)
+    inception_5a_pool = utils.conv2d_bn(inception_5a_pool,
+                                        layer='inception_5a_pool',
+                                        cv1_out=96,
+                                        cv1_filter=(1, 1),
+                                        padding=(1, 1))
+    inception_5a_1x1 = utils.conv2d_bn(inception_4e,
+                                       layer='inception_5a_1x1',
+                                       cv1_out=256,
+                                       cv1_filter=(1, 1))
+
+    inception_5a = concatenate([inception_5a_3x3, inception_5a_pool, inception_5a_1x1], axis=3)
+
+    # inception_5b
+    inception_5b_3x3 = utils.conv2d_bn(inception_5a,
+                                       layer='inception_5b_3x3',
+                                       cv1_out=96,
+                                       cv1_filter=(1, 1),
+                                       cv2_out=384,
+                                       cv2_filter=(3, 3),
+                                       cv2_strides=(1, 1),
+                                       padding=(1, 1))
+    inception_5b_pool = MaxPooling2D(pool_size=3, strides=2)(inception_5a)
+    inception_5b_pool = utils.conv2d_bn(inception_5b_pool,
+                                        layer='inception_5b_pool',
+                                        cv1_out=96,
+                                        cv1_filter=(1, 1))
+    inception_5b_pool = ZeroPadding2D(padding=(1, 1))(inception_5b_pool)
+
+    inception_5b_1x1 = utils.conv2d_bn(inception_5a,
+                                       layer='inception_5b_1x1',
+                                       cv1_out=256,
+                                       cv1_filter=(1, 1))
+    inception_5b = concatenate([inception_5b_3x3, inception_5b_pool, inception_5b_1x1], axis=3)
+
+    av_pool = AveragePooling2D(pool_size=(3, 3), strides=(1, 1))(inception_5b)
+    reshape_layer = Flatten()(av_pool)
+    dense_layer = Dense(128, name='dense_layer')(reshape_layer)
+    norm_layer = Lambda(lambda x: K.l2_normalize(x, axis=1), name='norm_layer')(dense_layer)
+
+    # Final Model
+    net_recognition = Model(inputs=[myInput], outputs=norm_layer)
+
+    # # Loading the model with pretrained weights
+    #
+    # FaceNet is trained by minimizing the triplet loss. I will be  loading a previously trained model. weights are avaiable at https://github.com/iwantooxxoox/Keras-OpenFace in the "weights" folder which is also provided in this source.
+    #
+    # This can take a couple of minutes to execute and depends on the speed of your machine.
+    #
+
+    # In[3]:
+
+    # Load weights from csv files (which was exported from Openface torch model)
+    weights = utils.weights
+    weights_dict = utils.load_weights()
+
+    # Set layer weights of the model
+    for name in weights:
+        if net_recognition.get_layer(name) != None:
+            net_recognition.get_layer(name).set_weights(weights_dict[name])
+        elif net_recognition.get_layer(name) != None:
+            net_recognition.get_layer(name).set_weights(weights_dict[name])
+
+    # OpenCV DNN supports 2 networks.
+    # 1. FP16 version of the original caffe implementation ( 5.4 MB )
+    # 2. 8 bit Quantized version using Tensorflow ( 2.7 MB )
+    DNN = "TF"
+    modelFile = "models/opencv_face_detector_uint8.pb"
+    configFile = "models/opencv_face_detector.pbtxt"
+    net_detection = cv2.dnn.readNetFromTensorflow(modelFile, configFile)
+
+    input_embeddings = create_input_image_embeddings(net_recognition)
+
+    conf_threshold = 0.7
+
+    source = 0
+    if len(sys.argv) > 1:
+        source = sys.argv[1]
+
+    cap = cv2.VideoCapture(source)
+    hasFrame, frame = cap.read()
+
+    frame_count = 0
+    tt_opencvDnn = 0
+    while(True):
+        hasFrame, frame = cap.read()
+        if not hasFrame:
+            break
+        frame_count += 1
+
+        height, width, channels = frame.shape
+
+        t = time.time()
+        outOpencvDnn, bboxes = detectFaceOpenCVDnn(net_detection,frame)
+        tt_opencvDnn += time.time() - t
+        fpsOpencvDnn = frame_count / tt_opencvDnn
+
+        for (x1, y1, x2, y2) in bboxes:
+            face_image = frame[max(0, y1):min(height, y2), max(0, x1):min(width, x2)]
+            identity = recognize_face(face_image, input_embeddings, net_recognition)
+
+            if identity is not None:
+                img = cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 255), 2)
+                cv2.putText(img, str(identity), (x1 + 5, y1 - 5), font, 1, (255, 255, 255), 2)
+
+        cv2.imshow("Face Detection Comparison", outOpencvDnn)
+
+        if frame_count == 1:
+            tt_opencvDnn = 0
+
+        k = cv2.waitKey(10)
+        if k == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
